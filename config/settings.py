973dd60d3341058d0c7b38bf4ff6008624cfa5cc
Yamil Nayen <yamil.nayen@inacapmail.cl>
2026-09-15 10:06:45 -0300

    commit: app, rutas, vistas y pagina inicial

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICS_DIR = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': TEMPLATES_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = STATICS_DIR
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
